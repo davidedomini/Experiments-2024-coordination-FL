c81cc64de1a3913a3dacb0c2d041629f13922eee
dom <davide.domini@unibo.it>
2024-02-10 11:44:15 +0100

    feat: add mlp

---
+++ spamemail/common/MLP.py
@@ -0,0 +1,15 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
